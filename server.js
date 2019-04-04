c5dd5ea42e44dfe58e5379b682c21890c78104a9
ambrose214 <ambrose.kuo@hotmail.com>
2019-04-04 13:23:27 -0400

    port change

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-var port = 8080 || process.env.PORT;
+var port = process.env.PORT || 8080;
 var server = require("http").Server(app);
 var io = require("socket.io").listen(server);
 
